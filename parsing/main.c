615ca31e9019ebe5e5607eb0e4320e235bca33a4
Josef El Ghnam <jel-ghna@c3r6p8.42vienna.com>
2025-08-05 21:36:20 +0200

    ADDED: parsing folder with basic shell

---
+++ parsing/main.c
@@ -0,0 +1,21 @@
+#include "minishell.h"
+
+int	main(void)
+{
+	char	*line;
+
+	while (1)
+	{
+		line = readline("josef-shell-$ ");
+		if (!line)
+			break ;
+		if (line[0])
+			add_history(line);
+		// rl_line_buffer[1] = 'x';
+		// printf("line read is \"%s\"\n", line);
+		// rl_on_new_line();
+		free(line);
+	}
+	printf("exit\n");
+	return (0);
+}
