44116209b30f33355f7e8241748daca97e5f984d
Michal Choma <mchoma@c3r1p6.42vienna.com>
2025-08-06 17:05:36 +0200

    changed echo

--- commands/echo.c
+++ commands/echo.c
@@ -6,7 +6,7 @@
 /*   By: mchoma <mchoma@student.42vienna.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/06 16:47:45 by mchoma            #+#    #+#             */
-/*   Updated: 2025/08/06 16:51:07 by mchoma           ###   ########.fr       */
+/*   Updated: 2025/08/06 17:05:14 by mchoma           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,12 +20,12 @@ int	ft_echo(t_data *data, char *str, int flag)
 	if (flag == 0 && printf("%s\n", str) == -1)
 	{
 		data->rt = 1;
-		return (free(str), set_rt(&data->rt, 0), 0);
+		return (free(str), set_rt(&data->rt, 1), 0);
 	}
 	if (flag == 1 && printf("%s", str) == -1)
 	{
 		data->rt = 1;
-		return (free(str), set_rt(&data->rt, 0), 0);
+		return (free(str), set_rt(&data->rt, 1), 0);
 	}
 	data->rt = 0;
 	return (free(str), set_rt(&data->rt, 0), 1);
